987d610f67ec6e6b05b432ba2bee8d4ff62aeffd
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-10-10 16:18:32 +0100

    feat: product list and card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+    </>
   );
 };
 
