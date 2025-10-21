1a4995f91b487d68564bd0eaca7f1d3a2a8e9490
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-10-21 17:40:38 +0100

    feat: next auth setup

---
+++ auth.ts
@@ -0,0 +1,71 @@
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/db/prisma';
+import Credentials from 'next-auth/providers/credentials';
+import { compareSync } from 'bcrypt-ts-edge';
+
+export const config = {
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      authorize: async (credentials) => {
+        if (credentials === null) return null;
+
+        // Find user in DB
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        // Check if the user exists and if the password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password,
+          );
+
+          // If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        // If user does not exist or the password does not match, return null
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token (jwt token)
+      session.user.id = token.sub;
+
+      // If there is an update (user can update their name on profile page), set the user name
+      if (trigger === 'update') {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
