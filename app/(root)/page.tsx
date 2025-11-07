d9bdbcaeaafaa8cfd619c7418aa9d250404a5afd
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-11-07 15:30:53 +0100

    feat: isr for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,9 @@ import { Suspense } from 'react';
 import Loader from '@/components/shared/loader';
 import ProductList from '@/components/shared/product/product-list';
 
+// Cache for 1 hour
+export const revalidate = 3600;
+
 const HomePage = () => {
   return (
     <>
