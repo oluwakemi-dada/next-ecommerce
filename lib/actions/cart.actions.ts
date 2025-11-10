fddbb27273169ab7a428e14f05df06fe4ddc9c2e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-11-10 20:27:12 +0100

    feat(cart): price calculation and add to database

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -3,8 +3,9 @@ import { cookies } from 'next/headers';
 import { auth } from '@/auth';
 import { prisma } from '@/db/prisma';
 import { CartItem } from '@/types';
-import { convertToPlainObject, formatError } from '../utils';
-import { cartItemSchema } from '../validators';
+import { convertToPlainObject, formatError, round2 } from '../utils';
+import { cartItemSchema, insertCartSchema } from '../validators';
+import { revalidatePath } from 'next/cache';
 
 export const getMyCart = async () => {
   // Check for the cart cookie
@@ -33,6 +34,23 @@ export const getMyCart = async () => {
   });
 };
 
+// Calculate cart prices
+const calcPrice = (items: CartItem[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0),
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + taxPrice + shippingPrice);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
+
 export const addItemToCart = async (data: CartItem) => {
   try {
     // Check for the cart cookie
@@ -54,17 +72,32 @@ export const addItemToCart = async (data: CartItem) => {
       where: { id: item.productId },
     });
 
-    console.log({
-      'Session Cart ID': sessionCartId,
-      'User ID': userId,
-      'Item Requested': item,
-      'Product Found': product,
-    });
+    if (!product) throw new Error('Product not found');
 
-    return {
-      success: true,
-      message: 'Item added to cart',
-    };
+    if (!cart) {
+      // Create new cart object
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
+
+      // Add to database
+      await prisma.cart.create({
+        data: newCart,
+      });
+
+      // Revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+
+      return {
+        success: true,
+        message: `${product.name} added to cart`,
+      };
+    } else {
+      // 
+    }
   } catch (error) {
     return {
       success: false,
