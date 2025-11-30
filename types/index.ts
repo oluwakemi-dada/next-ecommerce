07693a768579791521f2b4a7cb08f31671999cb6
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-11-30 10:52:24 +0100

    feat(types): add order Zod schema and TypeScript types

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,8 @@ import {
   cartItemSchema,
   shippingAddressSchema,
   variantSchema,
+  insertOrderSchema,
+  insertOrderItemSchema,
 } from '@/lib/validators';
 
 export type VariantInput = z.infer<typeof variantSchema>;
@@ -18,3 +20,17 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: {
+    name: string;
+    email: string;
+  };
+};
