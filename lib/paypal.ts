aa6ac773d96a56fed51a491823e3f015af613d6d
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-12-02 01:50:02 +0100

    feat(paypal): add create order and capture payment

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,54 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+const handleResponse = async (response: Response) => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+};
+
+export const paypal = {
+  createOrder: async (price: number) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'USD',
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+  capturePayment: async (orderId: string) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  },
+};
 
 // Generate paypal access token
 const generateAccessToken = async () => {
@@ -19,13 +67,8 @@ const generateAccessToken = async () => {
     },
   });
 
-  if (response.ok) {
-    const jsonData = await response.json();
-    return jsonData.access_token;
-  } else {
-    const errorMessage = await response.text();
-    throw new Error(errorMessage);
-  }
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
 };
 
 export { generateAccessToken };
