ddf09346cee3a2ecbf394f12ae5c07593288a157
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-12-18 11:09:23 +0100

    feat(search): add dynamic metadata

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -23,6 +23,45 @@ export type GetFilterUrlProps = {
   pg?: string;
 };
 
+type generateMetadataProps = {
+  searchParams: Promise<{
+    q: string;
+    category?: string;
+    price?: string;
+    rating?: string;
+  }>;
+};
+
+export const generateMetadata = async ({
+  searchParams,
+}: generateMetadataProps) => {
+  const {
+    q = 'all',
+    category = 'all',
+    price = 'all',
+    rating = 'all',
+  } = await searchParams;
+
+  const isQuerySet = q && q !== 'all' && q.trim() !== '';
+  const isCategorySet =
+    category && category !== 'all' && category.trim() !== '';
+  const isPriceSet = price && price !== 'all' && price.trim() !== '';
+  const isRatingSet = rating && rating !== 'all' && rating.trim() !== '';
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `Search ${isQuerySet ? q : ''} 
+      ${isCategorySet ? `: Category ${category}` : ''}
+      ${isPriceSet ? `: Price ${price}` : ''}
+      ${isRatingSet ? `: Rating ${rating}` : ''} `,
+    };
+  } else {
+    return {
+      title: 'Search Products',
+    };
+  }
+};
+
 const SearchPage = async (props: SearchPageProps) => {
   const {
     q = 'all',
