c73b7dddaf310a39d4ededfb89e6b1b36579c5ca
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-12-19 21:59:34 +0100

    feat(home): add icon boxes on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import Loader from '@/components/shared/loader';
 import ProductList from '@/components/shared/product/product-list';
 import FeaturedProducts from '@/components/shared/product/featured-products';
 import ViewAllProductsButton from '@/components/view-all-products-button';
+import IconBoxes from '@/components/icon-boxes';
 
 // Cache for 1 hour
 export const revalidate = 3600;
@@ -17,6 +18,7 @@ const HomePage = () => {
         <ProductList title="Newest Arrivals" limit={6} />
       </Suspense>
       <ViewAllProductsButton />
+      <IconBoxes />
     </>
   );
 };
