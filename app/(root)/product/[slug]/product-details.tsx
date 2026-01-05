382a838cc9eeb5ecc3780ea8d6dde8413b44c80c
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-01-05 13:13:48 +0100

    ui: increase grid gap between image and details on product details page

--- app/(root)/product/[slug]/product-details.tsx
+++ app/(root)/product/[slug]/product-details.tsx
@@ -14,7 +14,7 @@ const ProductDetails = async ({ slug }: ProductDetailsProps) => {
   if (!product) notFound();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
       <div>
         <ProductImages images={product.images} />
       </div>
